bb758248c21726c2e22e7a6715ff427979af2ba0
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2019-05-03 13:22:29 +0200

    Algo to generate card combinations

---
+++ generatePoints.php
@@ -0,0 +1,42 @@
+<?php
+
+function getConfigs()
+{
+    foreach (getDefense() as $defense) {
+        foreach (getAttacks() as $attacks) {
+            $pv = 35 - $defense - array_sum($attacks);
+            yield [$defense, $attacks[0], $attacks[1], $attacks[2], $pv];
+        }
+    }
+}
+
+function getDefense()
+{
+    yield from getCombinations(range(0,4));
+}
+
+function getAttacks()
+{
+    foreach (getCombinations([5,6,7]) as $a1) {
+        foreach(getCombinations(range($a1+1, 8)) as $a2) {
+            foreach(getCombinations(range($a2+1, 9)) as $a3) {
+                yield [$a1, $a2, $a3];
+            }
+        }
+    }
+}
+
+function getCombinations(array $possibleValues)
+{
+    foreach ($possibleValues as $value) {
+        yield $value;
+    }
+}
+
+
+$i=1;
+foreach(getConfigs() as $config) {
+    echo "[$i]\t" . implode(',', $config) . PHP_EOL;
+    ++$i;
+}
+
